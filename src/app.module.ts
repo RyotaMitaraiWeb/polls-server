4b14ab8dd5b001078eee62f18097d53b7a216ecd
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-11-05 18:21:39 +0200

    Add TypeORM and Postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '../variables.env',
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.HOST || 'localhost',
+            port: Number(process.env.PORT) || 5432,
+            username: process.env.POSTGRE_USERNAME || 'postgres',
+            password: process.env.DB_PASSWORD || '1234',
+            database: 'test',
+            synchronize: process.env.ENV === 'DEV',
+        }),
+
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
