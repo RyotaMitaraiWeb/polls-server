9b8b363950e53f6edb1d1e0f87f885c749fa55c7
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-11-24 16:33:26 +0200

    Update user module with the new JWT middlewares

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,19 +3,18 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity'
-import { Repository } from 'typeorm';
-import { BlacklistToken, VerifyLackOfToken, VerifyToken } from '../middlewares/jwt.middleware';
+import { BlacklistToken, CheckIfLogged, VerifyLackOfToken, VerifyToken } from '../middlewares/jwt.middleware';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
     controllers: [UserController],
-    providers: [UserService, Repository<User>, JwtService]
+    providers: [UserService, JwtService]
 })
 export class UserModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
         consumer
-            .apply(VerifyToken, BlacklistToken).forRoutes({
+            .apply(CheckIfLogged, VerifyToken, BlacklistToken).forRoutes({
                 path: 'user/logout', method: RequestMethod.DELETE,
             })
             .apply(VerifyLackOfToken).forRoutes({
@@ -24,7 +23,7 @@ export class UserModule implements NestModule {
             .apply(VerifyLackOfToken).forRoutes({
                 path: 'user/register', method: RequestMethod.POST,
             })
-            .apply(VerifyToken).forRoutes({
+            .apply(CheckIfLogged, VerifyToken).forRoutes({
                 path: 'user/isLogged', method: RequestMethod.POST,
             })
     }
