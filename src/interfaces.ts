68b2b49ec0fc9c188a3026a2095f764cadde864e
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-11-26 13:02:53 +0200

    make slight adjustments to the interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,36 +1,27 @@
 import { Request } from "express"
 import { Choice } from "./choice/entities/choice.entity"
 import { Poll } from "./poll/entities/poll.entity"
-import { User } from "./user/entities/user.entity"
 
-export interface IUserBody {
-    body: {
-        id: number,
-        username: string,
-        accessToken: string,
-    },
-}
 
-export interface IRequestError {
-    body: {
-        statusCode: number,
-        message: string[],
-        error: string,
-    }
-}
-
+/** Interface for user object that was decoded from a JWT token
+ * @property {number} id
+ * @property {string} username
+ */
 export interface DecodedToken {
     id: number,
     username: string,
 }
 
+/**Extends the Request interface from Express with these additional properties (which are attached via middlewares):
+ * @property {DecodedToken} user - user object decoded from a JWT token
+ * @property {boolean} isAuthor - indicates whether the user sending the request is the author of the entry or not
+ * @property {Poll} poll - this is the poll to which the request is made
+ * @property {boolean} isLogged - indicates whether the user is logged in or a guest
+ * @property {boolean} hasVoted - indicates whether the user has voted in the poll. Defaults to false if the user is a guest
+ * @property {number} voteId - the id of the choice that the user has voted, or -1 if the user has not voted or is a guest
+ */
 export interface IRequest extends Request {
-    username: string,
-    password: string,
     user: DecodedToken,
-    body: {
-        choices: string[],
-    },
     isAuthor: boolean,
     poll: Poll,
     isLogged: boolean,
@@ -38,11 +29,7 @@ export interface IRequest extends Request {
     voteId: number,
 }
 
-export interface IVoteCount {
-    content: string,
-    id: number,
-    count: number
-}
+/* INTERFACES FOR TESTS */
 
 export interface IPollBody {
     body: {
@@ -56,5 +43,28 @@ export interface IPollBody {
         hasVoted: boolean,
         voteCount: IVoteCount[],
         voteId: number,
+    }
+}
+
+export interface IUserBody {
+    body: {
+        id: number,
+        username: string,
+        accessToken: string,
+    }
+}
+
+export interface IVoteCount {
+    content: string,
+    id: number,
+    count: number
+}
+
+export interface IRequestError {
+    body: {
+        status: number,
+        message: string,
+        response: string,
+        error: string,
     }
 }
