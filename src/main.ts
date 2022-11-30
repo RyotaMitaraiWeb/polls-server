0aac1520164e32c513d253b9ed836b493db15976
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-11-30 11:08:25 +0200

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: process.env.CLIENT_PORT || '*',
+    });
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+    await app.listen(3000);
 }
 bootstrap();
